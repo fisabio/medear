4920c1f4ea51221fa5de542ded3a81a1f54f728d
Carlos Vergara-Hernández <carlos.vergara@uv.es>
2019-09-19 12:32:52 +0200

    Añadir propuesta de unión de seccionado para Barcelona

--- R/une_cambios.R
+++ R/une_cambios.R
@@ -459,6 +459,7 @@ une_secciones <- function(cambios = NULL, cartografia, poblacion = NULL, mortali
 
     cambios      <- cambios[between(year2, years_union[1], years_union[length(years_union)])]
     cambios$modo <- "auto"
+    cambios_copy <- copy(cambios)
 
     if (nrow(cambios) > 0) {
       islas_2011    <- sapply(cartografia@polygons, function(x) length(x@Polygons))
@@ -677,6 +678,9 @@ une_secciones <- function(cambios = NULL, cartografia, poblacion = NULL, mortali
       }
     }
     poblacion <- poblacion[order(seccion, sexo, year)]
+    if (datos_propios) {
+      poblacion <- poblacion[poblacion$year == 2011 & poblacion$seccion %in% cartografia$seccion]
+    }
     pob_array <- crea_cubo_poblacion(
       poblacion,
       periodo       = years_estudio,
@@ -763,16 +767,26 @@ une_secciones <- function(cambios = NULL, cartografia, poblacion = NULL, mortali
   ##############################################################################
 
   if (!is.null(censo)) {
-    # warning("El c\u00e1lculo del \u00edndice de privaci\u00f3n sigue la metodolog\u00eda MEDEA2.\n",
-    #         "V\u00e9ase: https://doi.org/10.1157/13123961", call. = FALSE)
     censo_c <- copy(censo)
     censo_c <- censo_c[muni %in% cartografia$CUMUN]
-
+    in_col  <- names(censo_c)[
+      !names(censo_c) %in% c("seccion", "muni", "year", "cluster")
+      ]
+    if (!is.null(cambios)) {
+      tmp <- attributes(cambios_copy)$equivalencia_barcelona$cluster_01
+      if (substr(tmp$sc, 1, 5)[1] == substr(cambios$sc_ref, 1, 5)[1]) {
+        for (i in seq_along(tmp$sc)) {
+          censo_c[
+            censo_c$year == 2001 & seccion == tmp$sc[i],
+            cluster := tmp$id_cluster_11[i]
+          ]
+        }
+        censo_c[censo_c$year == 2001, seccion := cluster]
+        censo_c[, cluster := NULL]
+      }
+    }
     censo_c[, cluster := cluster_sc[match(seccion, sc), id_cluster]]
     censo_c[is.na(cluster), cluster := seccion]
-    in_col <- names(censo_c)[
-      !names(censo_c) %in% c("seccion", "muni", "year", "cluster")
-    ]
     censo_c <- censo_c[
       ,
       lapply(.SD, sum),
@@ -780,33 +794,6 @@ une_secciones <- function(cambios = NULL, cartografia, poblacion = NULL, mortali
       .SDcols = in_col
     ][order(cluster, year)]
     setnames(censo_c, "cluster", "seccion")
-    # denominador <- paste0("i0", c(1:2, 1, 3:4, 5, 5, 5), "_d")
-    # numerador   <- paste0("i0", 1:8)
-    # vars_in     <- paste0("ind_", 1:8)
-    # bdd      <- mapply(
-    #   function(x, y) x / y * 100,
-    #   censo_c[, ..numerador],
-    #   censo_c[, ..denominador],
-    #   USE.NAMES = FALSE
-    # )
-    # dimnames(bdd) <- list(censo_c$seccion, vars_in)
-    # censo_c       <- cbind(censo_c, bdd)
-    # mi_fun <- function(datos, vars) {
-    #   mi_formu <- stats::as.formula(paste("~", paste(vars, collapse = " + ")))
-    #   indice   <- stats::prcomp(mi_formu, datos, center = TRUE, scale = TRUE)
-    #   indice   <- as.numeric(scale(indice$x[, 1]))
-    #   if (indice[which.max(rowSums(datos[, vars, with = FALSE]))] < 0)
-    #     indice <- -indice
-    #   return(indice)
-    # }
-    # censo_c <- censo_c[, indice := mi_fun(.SD, vars_in[1:5]), by = .(year, muni)]
-    # censo_c <- censo_c[order(year, seccion)]
-    # n_years <- length(unique(censo_c$year))
-    # indice  <- array(
-    #   dim      = c(length(unique(censo_c$seccion)), length(unique(censo_c$year))),
-    #   dimnames = list(unique(censo_c$seccion), unique(censo_c$year))
-    # )
-    # indice[, seq_len(n_years)] <- censo_c$indice
 
     res_attr        <- attributes(res)
     res             <- append(res, list(censo = censo_c))
